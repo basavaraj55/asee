31d385dc55aef1469253b31e4c06b28b86efc853
basavaraj55 <basavaraj.s2345@gmail.com>
2026-04-03 12:20:44 +0530

    Create alg.py

---
+++ alg.py
@@ -0,0 +1,15 @@
+# prime_analysis.py
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+primes = [n for n in range(1, 101) if is_prime(n)]
+
+print("Prime numbers between 1 and 100:")
+print(primes)
+print("Total primes:", len(primes))
